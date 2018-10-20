47c0fe7c133b8fb9602f2db31c1b652e105bad82
Zhisen J. Urgolites <zhisen.urgolites@gmail.com>
2018-10-20 13:47:29 -0700

    add python file that process raw data to become filtered data

---
+++ Raw Data/raw_to_filtered.py
@@ -0,0 +1,41 @@
+import pandas as pd
+import numpy as np
+import matplotlib as plt
+from itertools import groupby
+
+df = pd.read_csv('cleaned_data.zip')
+df = df.apply(pd.to_numeric, errors = 'ignore')
+print(df.count())
+
+mean_G = df.G.mean()
+sd_G = df.G.std()
+low_limit = meanG -2*sdG
+high_limit = meanG + 2*sdG
+print(mean_G)
+print(sd_G)
+print(low_limit)
+print(high_limit)
+
+df['outlier'] = (df['G'] <= low_limit) | (df['G'] >= high_limit)
+print(df.head())
+
+outlier = df['outlier']
+trues = []
+index_list = []
+
+for index, value in outlier.iteritems():
+    if value:
+        trues.append(index)
+    else:
+        if len(trues) > 1:
+            index_list.extend(trues)
+        trues = []
+
+if len(trues) > 1:
+    index_list.extend(trues)
+
+df_filtered = df.iloc[index_list]
+print(df_filtered.count())
+print(df_filtered.head())
+
+df_filtered.to_csv('filtered.csv')
