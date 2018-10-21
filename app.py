d60fe4dc422e53300e83033226109eaa59def2e9
Erikcaldwell <39847875+Erikcaldwell@users.noreply.github.com>
2018-10-20 20:15:54 -0700

    Fixing Erik's Mistakes
    Fixed the API in the ap.py file.  It actually wasn't an API now it is.  So you can get the lat / lng for all the data now at /api/location.  Should now be able to do a GET request for that too and pull into into Javascript or something else.  If this isn't working for someone please let me know.  I also updated the /data so that the template is called data.html to avoid confusion with our index.html Daniel is making.

--- app.py
+++ app.py
@@ -3,6 +3,10 @@ import pymongo
 from flask import Flask, render_template, redirect
 import flask
 from flask_pretty import Prettify
+from flask import Flask
+from flask import jsonify
+from flask import request
+
 
 # Create flask app
 app = Flask(__name__)
@@ -18,13 +22,32 @@ collection = db.road_data
 
 road_data = list(db.road_data.find())
 
-
 # Create root
 @app.route('/data')
 def index():
     road_data = list(db.road_data.find())
-    return render_template('index.html', road_data=road_data)
+    return render_template('data.html', road_data=road_data)
 
+#RESTAPI code starts here
+@app.route('/api/location', methods=['GET'])
+def get_all_road_data():
+  output = []
+  for s in db.road_data.find():
+    output.append({'Latitude' : s['Latitude'], 'Longitude' : s['Longitude']})
+  return jsonify({'result' : output})
 
+#experimental code:  want to allow us to pull only the location records for a certain driverself.
+#this code currently does not work...
+@app.route('/api/driver', methods=['GET'])
+def get_one_driver(driver):
+  road_data = db.road_data
+  s = road_data.find_one({'driver' : driver})
+  if s:
+    output = {'driver' : s['driver'],'Latitude' : s['Latitude'], 'Longitude' : s['Longitude']}
+  else:
+    output = "No such name"
+  return jsonify({'result' : output})
+
+#this code works.
 if __name__ == '__main__':
     app.run(debug=True)
